7eabeb260ea53b55f0c38e586c7be45cfb697ee5
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2025-10-24 22:32:18 +0600

    basic layout logincreated

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,22 @@
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Root from "./Layout/Root";
+import { Children } from "react";
+import Home from "./components/Home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: Root,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      }
+    ],
   },
+
 ]);
 
 const root = document.getElementById("root");
